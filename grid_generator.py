a72f7ef178adf1433f1e3421e15cc4774afeb946
hmhuy00 <hmhuy00@gmail.com>
2023-01-14 01:04:41 +0800

    add

--- grid_generator.py
+++ grid_generator.py
@@ -1,36 +1,95 @@
 import numpy as np
 from PIL import Image
+import os
+import glob
+from copy import deepcopy
+from tqdm import trange
 
-def padding(img,img_size):
-    toggle = 0
-    while img.shape[0]<img_size:
-        if (toggle == 0):
-            img = np.concatenate((img,np.zeros((1,img.shape[1]),dtype=np.uint8)),0)
-        else:
-            img = np.concatenate((np.zeros((1,img.shape[1]),dtype=np.uint8),img),0)
-        toggle = 1 - toggle
-    toggle = 0
-    while img.shape[1]<img_size:
-        if (toggle == 0):
-            img = np.concatenate((img,np.zeros((img.shape[0],1),dtype=np.uint8)),1)
-        else:
-            img = np.concatenate((np.zeros((img.shape[0],1),dtype=np.uint8),img),1)
-        toggle = 1 - toggle
-    return img
+WALL_VALUE = 200
+root = './maze_dataset'
+try:
+    os.mkdir(root)
+except:
+    print(f'{root} existed')
+    raise
 
-def create_empty_grid(grid_size,scale=3):
-    grid = np.zeros((grid_size*scale,grid_size*scale),dtype=np.uint8)
-    grid[:,0:scale] = 200
-    grid[:,-scale:] = 200
-    grid[0:scale,:] = 200
-    grid[-scale:,:] = 200
+def create_random_room(old_grid):
+    grid = deepcopy(old_grid)
+    (grid_size,grid_size) = grid.shape
+    posX,posY = None,None
+
+    list_position_prompts = [
+        'add a in the top left',
+        'add a in the top right',
+        'add a in the bottom left',
+        'add a in the bottom right',
+    ]
+    position_promt = np.random.choice(range(len(list_position_prompts)))
+    if position_promt == 0:
+        posX = np.random.randint(low=0,high=grid_size//2)
+        posY = np.random.randint(low=0,high=grid_size//2)
+    elif position_promt == 1:
+        posX = np.random.randint(low=0,high=grid_size//2)
+        posY = np.random.randint(low=grid_size//2,high=grid_size)
+    elif position_promt == 2:
+        posX = np.random.randint(low=grid_size//2,high=grid_size)
+        posY = np.random.randint(low=0,high=grid_size//2)
+    elif position_promt == 3:
+        posX = np.random.randint(low=grid_size//2,high=grid_size)
+        posY = np.random.randint(low=grid_size//2,high=grid_size)
+
+    #---------------------------------------------------------#        
+    size = np.random.randint(low=1,high=grid_size//2)
+    #---------------------------------------------------------#        
+    for x in range(grid_size):
+        for y in range(grid_size):
+            if (abs(x-posX)+abs(y-posY)==size):
+                grid[x,y] = WALL_VALUE
+
+    final_promt = list_position_prompts[position_promt] + f' with size {size}.'
+    return grid,final_promt
+
+def create_empty_grid(grid):
+    grid[:,0:1] = WALL_VALUE
+    grid[:,-1:] = WALL_VALUE
+    grid[0:1,:] = WALL_VALUE
+    grid[-1:,:] = WALL_VALUE
 
     return grid
 
-def main(grid_size):
-    grid = create_empty_grid(grid_size)
+def save_grid(grid,id):
+    name = f'{id}'
+    while len(name)<7:
+        name = '0'+name
     im = Image.fromarray(grid)
-    im.save(f"test_figs/empty_{grid_size}.png")
+    im.save(f"{root}/{name}.png")
+    id += 1
+    return id
+
+def main(grid_size):
+    csv_file = open('dataset.csv','w')
+    current_id = 0
+    grid = np.zeros((grid_size,grid_size),dtype=np.uint8)
+    current_id = save_grid(grid,current_id)
+    csv_file.write(f'0000000.png,initialize grid.,0000001.png\n')
+    grid = create_empty_grid(grid)
+    current_id = save_grid(grid,current_id)
+    for loop in range(1):
+        list_file = glob.glob(f"{root}/*.png")
+        for idx in range(len(list_file)):
+            if (list_file[idx] == f'{root}/0000000.png'):
+                list_file.pop(idx)
+                break
+        for time in trange(10000):
+            random_file = np.random.choice(list_file)
+            grid = np.asarray(Image.open(random_file))
+            new_grid, promt = create_random_room(grid)
+            name = f'{current_id}'
+            while len(name)<7:
+                name = '0'+name
+            csv_file.write(f'{random_file.split("/")[-1]},{promt},{name}.png\n')
+            current_id = save_grid(new_grid,current_id)
+    csv_file.close()
 
 if __name__ == '__main__':
     for size in [16]:
